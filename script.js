43c7e9030d87cfb312053ca6fc15b4afc158a8a5
folluman <kauebastos74@gmail.com>
2024-10-14 22:37:40 -0300

    Site finalizado com LoggingIn

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const menu_burger = document.querySelector('.menu_hamburguer')
 const links = document.querySelector('.menu_hamburguer__links')
+const loggingIn = document.querySelector('.loggingIn')
+const site = document.querySelector('.site')
+
 ScrollReveal({ reset: true });
 
-menu_burger.addEventListener('click', function() {
-    links.classList.toggle('hidden')
-})
+setTimeout(() => {
+    loggingIn.classList.add('hidden__loggingIn');
+}, 5000)
 
-ScrollReveal().reveal('.apresentacao_animation', { 
-    delay: 1000,
- })
+setTimeout(() => {
+    site.classList.remove('hidden__site')
 
-ScrollReveal().reveal('.apresentacao__sobre , .apresentacao_skills', { 
-    duration: 2000,
-    distance: '90px',
- });
+    menu_burger.addEventListener('click', function() {
+        links.classList.toggle('hidden')
+    })
+    
+    ScrollReveal().reveal('.apresentacao_animation', { 
+        delay: 1000,
+     })
+    
+    ScrollReveal().reveal('.apresentacao__sobre , .apresentacao_skills', { 
+        duration: 2000,
+        distance: '90px',
+     });
+    
+    ScrollReveal().reveal('.projetos', {
+        delay: 500,
+    })
 
-ScrollReveal().reveal('.projetos', {
-    delay: 500,
-})
+}, 6000)
